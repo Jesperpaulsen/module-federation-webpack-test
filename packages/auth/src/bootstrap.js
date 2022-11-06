8fea9ee6719727f20ed9007f0a5ad05d2b4aafb2
Jesper Paulsen <jesper@jesper.no>
2022-11-06 17:07:15 +0100

    add auth module

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
+import App from './App'
+
+const mount = (el, { onNavigate, defaultHistory, initialPath } = {}) => {
+
+  const history = defaultHistory ?? createMemoryHistory({ 
+    initialEntries: [initialPath] 
+  })
+
+  if (onNavigate) history.listen(onNavigate)
+
+  ReactDOM.render(
+    <App history={history} />,
+    el
+  )
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      if (nextPathname === history.location.pathname) {
+        return
+      }
+      history.push(nextPathname)
+    }
+  };
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#_auth_dev-root')
+  if (el) {
+    mount(el, { defaultHistory: createBrowserHistory() })
+  }
+}
+
+export { mount }
