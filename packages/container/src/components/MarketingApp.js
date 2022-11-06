a8a7c659be89a79c2c3ce7abbfb75661c94a7438
Jesper Paulsen <jesper@jesper.no>
2022-11-06 17:07:03 +0100

    fix public path bug and history fallback

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -9,6 +9,7 @@ const MarketingApp = () => {
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         if (history.location.pathname === nextPathname) {
           return
